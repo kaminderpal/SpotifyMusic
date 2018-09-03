8ae0861f5b1f3751bb2fef3c5cf96ceecc6832d2
kaminderpal <kaminderpal@livebarn.com>
2018-09-03 12:58:03 -0400

    add react router config and materialize css, font awesome

---
+++ src/Router/router.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react'
+import {Route,Switch} from 'react-router-dom';
+export class Router extends Component {
+  render() {
+    return (
+      <Fragment>
+       
+      </Fragment>
+    )
+  }
+}
+
+export default Router
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../node_modules/materialize-css/dist/css/materialize.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (
+     <BrowserRouter>
+          <App/>
+     </BrowserRouter>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
